bbff21896eb02f39b60459d1f0fad1fdfc4b6b56
leowjunhao <JHLEOW.21@ichat.sp.edu.sg>
2023-01-30 11:41:35 +0800

    second commit

--- app/src/main/java/sp/com/FirstPage.java
+++ app/src/main/java/sp/com/FirstPage.java
@@ -3,11 +3,15 @@ package sp.com;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class FirstPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_page);
     }
