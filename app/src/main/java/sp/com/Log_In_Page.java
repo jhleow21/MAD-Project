fa3d307522faa0d0754716e9fd9c715460fec257
leowjunhao <JHLEOW.21@ichat.sp.edu.sg>
2023-01-31 16:33:16 +0800

    Login

---
+++ app/src/main/java/sp/com/Log_In_Page.java
@@ -0,0 +1,14 @@
+package sp.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Log_In_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.log_in_page);
+    }
+}
