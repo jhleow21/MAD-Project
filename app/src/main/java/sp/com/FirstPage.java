d9efdfdee960101c709c4867beb2b31fb7097f48
leowjunhao <JHLEOW.21@ichat.sp.edu.sg>
2023-01-31 15:53:13 +0800

    -m "Sign up page"

--- app/src/main/java/sp/com/FirstPage.java
+++ app/src/main/java/sp/com/FirstPage.java
@@ -2,10 +2,16 @@ package sp.com;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class FirstPage extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +20,15 @@ public class FirstPage extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_page);
+
+        Button SignUp = findViewById(R.id.signup);
+        SignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FirstPage.this, Sign_Up_Page.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
