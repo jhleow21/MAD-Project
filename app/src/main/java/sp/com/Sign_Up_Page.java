8339f226126b4ef720f846cc2a6f663df582205a
leowjunhao <JHLEOW.21@ichat.sp.edu.sg>
2023-01-31 15:55:12 +0800

    -m "Sign up page"

---
+++ app/src/main/java/sp/com/Sign_Up_Page.java
@@ -0,0 +1,14 @@
+package sp.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sign_Up_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up_page);
+    }
+}
